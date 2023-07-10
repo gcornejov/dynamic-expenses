4763ed70390e133b8b601c6ffeb4a5438bc7f245
Gustavo Cornejo <g.cornejovivado@gmail.com>
2023-07-10 05:54:46 +0000

    Refactor method to load more resources

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,17 @@
 import json
 
 
-def load_groups():
+def load_data():
     groups = {}
-    with open("src/resources/groups.json") as f:
-        groups = json.load(f)
-    return groups
+    expenses = {}
+
+    with open("src/resources/groups.json") as gp, open(
+        "src/resources/expenses.json"
+    ) as ex:
+        groups = json.load(gp)
+        expenses = json.load(ex)
+    return groups, expenses
 
 
 if "__name__" == "__name__":
-    print(load_groups())
+    print(load_data())
