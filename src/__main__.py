b45f84ec59b5c0965845885014719b789432131b
Gustavo Cornejo <g.cornejovivado@gmail.com>
2023-07-18 04:41:55 +0000

    Add compute amount and percentages logic

--- src/__main__.py
+++ src/__main__.py
@@ -14,14 +14,29 @@ def load_data():
     return groups, expenses
 
 
-def compute_percentajes(groups, expenses):
+def compute_percentages(groups, expenses):
     tot = sum(item["amount"] for item in expenses)
 
     locale.setlocale(locale.LC_MONETARY, "es_CL.UTF-8")
     locale._override_localeconv = {"frac_digits": 0}
     # print(locale.localeconv()["frac_digits"])
-    print(locale.currency(tot, symbol=True, grouping=True))
+    # print(locale.currency(tot, symbol=True, grouping=True))
+
+    summ = {}
+    for ex in expenses:
+        grp = ex["group"]
+        amnt = ex["amount"]
+
+        summ[grp] = summ.get(grp, 0) + amnt
+
+    grouped_ammounts = {
+        k: locale.currency(v, symbol=True, grouping=True) for (k, v) in summ.items()
+    }
+    percentages = {k: f"{v/tot:.2%}" for (k, v) in summ.items()}
+
+    print(grouped_ammounts)
+    print(percentages)
 
 
 if "__name__" == "__name__":
-    compute_percentajes(*load_data())
+    compute_percentages(*load_data())
