c4349b25bd575613e0a89872a45edbdfb8549d74
Gustavo Cornejo <g.cornejovivado@gmail.com>
2023-07-18 03:54:39 +0000

    Add calculate_percentajes function

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,5 @@
 import json
+import locale
 
 
 def load_data():
@@ -13,5 +14,14 @@ def load_data():
     return groups, expenses
 
 
+def compute_percentajes(groups, expenses):
+    tot = sum(item["amount"] for item in expenses)
+
+    locale.setlocale(locale.LC_MONETARY, "es_CL.UTF-8")
+    locale._override_localeconv = {"frac_digits": 0}
+    # print(locale.localeconv()["frac_digits"])
+    print(locale.currency(tot, symbol=True, grouping=True))
+
+
 if "__name__" == "__name__":
-    print(load_data())
+    compute_percentajes(*load_data())
