3fd49f31ee985e705571616cb0c1b84eb417ad09
Gustavo Cornejo <g.cornejovivado@gmail.com>
2023-07-19 03:03:50 +0000

    Move total expenes calculation

--- src/__main__.py
+++ src/__main__.py
@@ -15,7 +15,7 @@ def load_data():
 
 
 def compute_percentages(groups, expenses):
-    tot = sum(item["amount"] for item in expenses)
+    # tot = sum(item["amount"] for item in expenses)
 
     locale.setlocale(locale.LC_MONETARY, "es_CL.UTF-8")
     locale._override_localeconv = {"frac_digits": 0}
@@ -23,11 +23,13 @@ def compute_percentages(groups, expenses):
     # print(locale.currency(tot, symbol=True, grouping=True))
 
     summ = {}
+    tot = 0
     for ex in expenses:
         grp = ex["group"]
         amnt = ex["amount"]
 
         summ[grp] = summ.get(grp, 0) + amnt
+        tot = tot + amnt
 
     grouped_ammounts = {
         k: locale.currency(v, symbol=True, grouping=True) for (k, v) in summ.items()
