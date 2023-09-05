f45464279b0a644e6ee0252d6cf010ad32d1e6f8
Gustavo Cornejo <g.cornejovivado@gmail.com>
2023-09-05 05:27:58 +0000

    Reformat input file path

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import pandas as pd
 
-DATA_FILENAME = "src/resources/Saldo_y_Mov_No_Facturado.xls"
+INPUT_FILES_DIR = "input_data"
+DATA_FILENAME = "Saldo_y_Mov_No_Facturado.xls"
 
 ACCOUNT_AMOUNT = 1254994
 FIXED_WEEK_MONEY = 120000
@@ -35,7 +38,7 @@ if "__name__" == "__name__":
     # Load data from bank excel (Banco de Chile - National Credit)
     # raw_data.iloc[17:,[4,10]]
     expenses_df = pd.read_excel(
-        DATA_FILENAME,
+        Path(INPUT_FILES_DIR) / DATA_FILENAME,
         sheet_name=0,
         header=17,
         names=["Date", "Description", "Amount"],
